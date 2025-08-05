f9436ea9d410b431d85f81f15c718fbc8075c917
Priyansh   Dhiman <Priyansh.dhiman25@gmail.com>
2025-08-04 21:03:00 -0500

    A game where you fill random words to create a story.

---
+++ MadLibs_Game.py
@@ -0,0 +1,14 @@
+#Mad Libs Game
+
+
+adjective1 = input("Add an adjective (Description of something): ")
+noun1 = input("Add a noun (Person,place,or thing): ")
+adjective2 = input("Add an adjective (Description of something): ")
+verb1 = input("Enter a verb ending with 'ing' (Action word): ")
+adjective3 = input("Add an adjective (Description of something): ")
+
+
+print(f"Today I went a {adjective1} Zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
