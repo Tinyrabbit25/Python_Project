09203d2a52f7f8939ade073c94a4e60be58ba033
Priyansh   Dhiman <Priyansh.dhiman25@gmail.com>
2025-08-06 20:35:58 -0500

    Created two programs: Program 1 : Convers Weight by whatever number you give it and you can only convert it from Kgs -> Lbs || Lbs -> Kgs Program 2: Same as one but a temperature conversion program where it convers °C -> °F || °F-> °C.

---
+++ Weight_converter.py
@@ -0,0 +1,17 @@
+# Weight converter program
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilograms or Pounds (K or L): ")
+
+if unit == "K":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"Your Weight is: {round(weight,1)} {unit}")
+elif unit == "L":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"Your Weight is: {round(weight,1)} {unit}")
+else:
+    print(f"{unit} was not Valid!")
+
+
